70944d81d6dc21cc607f93b2a6cf374c75aa0703
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-11 07:11:10 +0700

    #8 - Set Seeder ✏️📗:octocat:⬆

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Article;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        //seed users table
+        User::factory(2)->create();
+        //seed articles table
+        Article::factory(50)->create();
     }
 }
