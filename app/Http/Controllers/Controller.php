3902161eb9467b6831654a00c74132fa79f6c1bb
Hendi Santika <hendisantika@yahoo.co.id>
2023-10-11 07:13:04 +0700

    #9 - Set Swagger Details ✏️📗:octocat:⬆

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,25 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Articles API Documentation",
+ *     description="Articles API Documentation",
+ *     @OA\Contact(
+ *         name="Hendi Santika",
+ *         email="hendisantika@yahoo.co.id"
+ *         link="https://s.id/hendisantika"
+ *     ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * ),
+ * @OA\Server(
+ *     url="/api/v1",
+ * ),
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
